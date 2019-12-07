2da75535024c9155f729f77c2501e482fd6f25dc
i0sch4in <i0sch4in@gmail.com>
2019-12-07 16:35:40 +0100

    addFilmToXML.php

--- php/addFilm.php
+++ php/addFilm.php
@@ -1,19 +1,19 @@
 <?php include "../php/navbar.php" ?>
     <div class="container align-items-center">
-      <form id="registerForm" action="../php/addFilmToDB.php" method="POST">
+      <form id="filmForm" action="../php/addFilmToXML.php" method="POST">
         <div class="form-group">
           <div class="input-group mb-3">
             <div class="input-group-prepend">
               <span class="input-group-text" id="basic-addon1">egilearen e-posta</span>
             </div>
-            <input type="text" name="eposta" id="eposta" class="form-control" value="<?php echo $_SESSION['eposta'] ?>" disabled>
+            <input type="text" name="eposta" id="eposta" class="form-control" value="<?php echo $_SESSION['eposta'] ?>" readonly>
           </div>
           <div class="form-group">
             <div class="input-group mb-3">
               <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">Izenburua</span>
               </div>
-              <input type="text" name="izenburua" id="izenburua" class="form-control" placeholder="Titanic">
+              <input type="text" name="izenburua" id="izenburua" class="form-control">
             </div>
           </div>
           <div class="form-group">
@@ -21,7 +21,7 @@
               <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">Urtea</span>
               </div>
-              <input type="text" name="urtea" id="urtea" class="form-control" placeholder="1997">
+              <input type="text" name="urtea" id="urtea" class="form-control">
             </div>
           </div>
           <div class="form-group">
@@ -29,7 +29,7 @@
               <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">Zuzendaria</span>
               </div>
-              <input type="text" name="zuzendaria" id="zuzendaria" class="form-control" placeholder="James Cameron">
+              <input type="text" name="zuzendaria" id="zuzendaria" class="form-control">
             </div>
           </div>
           <div class="form-group">
@@ -37,7 +37,7 @@
               <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">Generoa</span>
               </div>
-              <input type="text" name="generoa" id="generoa" class="form-control" placeholder="Drama">
+              <input type="text" name="generoa" id="generoa" class="form-control">
             </div>
           </div>
           <div class="form-group">
@@ -45,7 +45,7 @@
               <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">Iraupena (min)</span>
               </div>
-              <input type="text" name="iraupena" id="iraupena" class="form-control" placeholder="195">
+              <input type="text" name="iraupena" id="iraupena" class="form-control">
             </div>
           </div>
           <div class="form-group">
@@ -53,7 +53,15 @@
               <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">Sinopsia</span>
               </div>
-              <textarea name="sinopsia" id="sinopsia" class="form-control" placeholder='A 100-year-old woman named Rose DeWitt Bukater tells a story about her voyage on the famous ship Titanic. She is sharing the story with her granddaughter, Lizzy Calvert, and a crew of men who are interested in the Titanic shipwreck.'></textarea>
+              <textarea name="sinopsia" id="sinopsia" class="form-control" ></textarea>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">Balorazioa (0..100)</span>
+              </div>
+              <input type="number" name="balorazioa" id="balorazioa" class="form-control" min="0" max="100" value="1">
             </div>
           </div>
           <div class="form-group">
@@ -64,7 +72,7 @@
               <input type="text" name="esteka" id="esteka" class="form-control" placeholder="https://www.youtube.com/embed/FiRVcExwBVA">
             </div>
           </div>
-          <button type="sumbit" id="bidali" class="btn btn-danger" disabled>Bidali</button>
+          <button id="bidali" class="btn btn-danger" disabled>Bidali</button>
       </form>
       <div id="mezua">
       </div>
@@ -90,14 +98,15 @@
             }
           });
           $(".form-control").change(function(){
-            if($("#mezua").html() == ""){
+            if(($("#mezua").html() == "")){
               $("#bidali").prop("disabled", false);
             }
             else{
-              $("#bidali").prop("disabled", true);
+                $("#bidali").prop("disabled", true);
             }
           });
         });
+
       </script>
   </body>
 </html>
--- php/addFilmToXML.php
+++ php/addFilmToXML.php
@@ -1,26 +1,37 @@
 <?php
-$xml = simplexml_load_file('../xml/filmak.xml');
-# Ezin dira urte eta izenburu bereko bi film egon
-foreach($xml -> children() as $filma){
-  if($filma['izenburua'] == $_POST['izenburua'] && $filma['urtea'] == $_POST['urtea']){
-    $exists = true
+include "../php/navbar.php"
+?>
+<div class="container">
+  <?php
+  $xml = simplexml_load_file('../xml/filmak.xml');
+  # Ezin dira urte eta izenburu bereko bi film egon
+  foreach($xml -> children() as $filma){
+    if($filma['izenburua'] == $_POST['izenburua'] && $filma['urtea'] == $_POST['urtea']){
+      $exists = true;
+    }
   }
-}
-if($exists){
-  die('filmExists');
-}
-else{
-  $filma = $xml -> addChild('filma');
-  $filma -> addChild('egilea', $_POST['egilea']);
-  $filma -> addChild('izenburua', $_POST['izenburua']);
-  $filma -> addChild('urtea', $_POST['urtea']);
-  $filma -> addChild('zuzendaria', $_POST['zuzendaria']);
-  $filma -> addChild('generoa', $_POST['generoa']);
-  $filma -> addChild('iraupena', $_POST['iraupena']);
-  $filma -> addChild('sinopsia', $_POST['sinopsia']);
-  $filma -> addChild('balorazioa', $_POST['balorazioa']);
-  $filma -> addChild('esteka', $_POST['esteka']);
+  if(isset($exists)){
+    echo "<p class='text-danger'><span class='font-weight-bold'>ERROREA:</span> katalogoan honezkero badago". $_POST['izenburua']." (".$_POST['urtea'].")! </p>";
+    echo "<p> Beste film bat sartu nahi baduzu egin klik <a href='../php/addFilm.php'>hemen</a> </p>";
+    exit();
+  }
+  else{
+    $filma = $xml -> addChild('filma');
+    $filma -> addChild('egilea', $_POST['eposta']);
+    $filma -> addChild('izenburua', $_POST['izenburua']);
+    $filma -> addChild('urtea', $_POST['urtea']);
+    $filma -> addChild('zuzendaria', $_POST['zuzendaria']);
+    $filma -> addChild('generoa', $_POST['generoa']);
+    $filma -> addChild('iraupena', $_POST['iraupena']);
+    $filma -> addChild('sinopsia', $_POST['sinopsia']);
+    $filma -> addChild('balorazioa', $_POST['balorazioa']);
+    $filma -> addChild('esteka', $_POST['esteka']);
 
-  $xml -> asXML('../xml/filmak.xml');
-}
+    $xml -> asXML('../xml/filmak.xml');
+    echo "<p>". $_POST['izenburua']." (".$_POST['urtea'].") katalogora gehitu da. Katalogo osoa ikusi nahi baduzu egin klik <a href='../php/showFilms.php'>hemen</a> </p>";
+  }
+  ?>
+</div>
+<?php
+include "../php/footer.php";
 ?>
